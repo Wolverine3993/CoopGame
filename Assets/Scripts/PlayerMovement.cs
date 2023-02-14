fe63a58ab40e21add87c8506075e9edf035dec03
Wolverine3993 <oscar@koopmanmail.com>
2023-02-14 22:11:04 +1100

    aerifhikesuarht

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,6 +8,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpHeight;
+    [SerializeField] LayerMask groundLayer;
+    float xInput;
+    float yInput;
+    Rigidbody2D rb;
+    Collider2D collander;
+    private delegate void HandleMove();
+    HandleMove Move;
     enum PlayerNumber
     {
         Player1,
@@ -15,16 +23,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] PlayerNumber playerNumber = new PlayerNumber();
     void Start()
     {
+        Move = Player1Move;
+        if (playerNumber.ToString() == "Player2")
+        {
+            Move = Player2Move;
+            groundLayer += LayerMask.GetMask("Player1");
+        }
+        else
+            groundLayer += LayerMask.GetMask("Player2");
+        rb = GetComponent<Rigidbody2D>();
+        collander = GetComponent<Collider2D>();
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(playerNumber.ToString());
+        Move();
+        if(TouchingGround() && yInput > 0)
+            Jump();
     }
     void Player1Move() 
-    { 
-    
+    {
+        xInput = Input.GetAxisRaw("Player1Horizontal");
+        yInput = Input.GetAxisRaw("Player1Vertical");
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+    void Player2Move()
+    {
+        xInput = Input.GetAxisRaw("Player2Horizontal");
+        yInput = Input.GetAxisRaw("Player2Vertical");
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+    bool TouchingGround()
+    {
+        return Physics2D.BoxCast(new Vector2(transform.position.x, transform.position.y - collander.bounds.size.y / 2), new Vector2(collander.bounds.size.x - 0.1f, 0.1f), 0, Vector2.down, 0.1f, groundLayer) != false;        
     }
 }
